d2b6bc7e5be23e73c3639f1aa268712c51ebc018
Aniket Rathi <aniketrathi897@gmail.com>
2021-03-05 20:18:52 -0500

    added get specific post router

--- server/routes/post-route.js
+++ server/routes/post-route.js
@@ -25,3 +25,24 @@ router.get("/posts", check, (req, res) => {
     res.status(500).send();
   }
 });
+
+router.get("/posts/:postid", check, (req, res) => {
+  const { postid } = req.params;
+  try {
+    Post.findById(req.params.postid)
+      .lean()
+      .populate("author")
+      .then((post) => {
+        if (!post) {
+          return res.status(404).json({
+            message: "No post with such id found.",
+          });
+        }
+
+        return res.json(post);
+      });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
