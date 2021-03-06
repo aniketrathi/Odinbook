028ca8feaa38ca15cdf140290ac7b2ab33ba71bf
Aniket Rathi <aniketrathi897@gmail.com>
2021-03-06 16:19:50 -0500

    added delete comments router

--- server/routes/comment-route.js
+++ server/routes/comment-route.js
@@ -38,7 +38,7 @@ router.post(
     const { postid } = req.params;
     const { content } = req.body;
     try {
-      const errors = validationResult(req);
+      const errors = await validationResult(req);
 
       if (!errors.isEmpty()) {
         return res.status(400).json({
@@ -71,27 +71,53 @@ router.post(
   }
 );
 
-router.put("/posts/:postid/comments/:commentid", check, (req, res) => {
-  const { commentid } = req.params;
-  try {
-    const errors = validationResult(req);
+router.put(
+  "/posts/:postid/comments/:commentid",
+  check,
+  commentValidator,
+  async (req, res) => {
+    const { commentid } = req.params;
+    try {
+      const errors = await validationResult(req);
 
-    if (!errors.isEmpty()) {
-      return res.status(400).json({
-        message: "Bad request.",
-        details: errors.array(),
-      });
-    }
-
-    const updatedData = { ...req.body };
-
-    Comment.updateOne({ _id: commentid }, updatedData).then((updateResult) => {
-      if (updateResult.nModified !== 1) {
-        throw new Error("Update result did not return nModified as 1");
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          message: "Bad request.",
+          details: errors.array(),
+        });
       }
 
-      return res.json({ ...updatedData, _id: commentid });
-    });
+      const updatedData = { ...req.body };
+
+      await Comment.updateOne({ _id: commentid }, updatedData).then(
+        (updateResult) => {
+          if (updateResult.nModified !== 1) {
+            throw new Error("Update result did not return nModified as 1");
+          }
+
+          return res.json({ ...updatedData, _id: commentid });
+        }
+      );
+    } catch (err) {
+      console.error(err);
+      res.status(500).send();
+    }
+  }
+);
+
+router.delete("/posts/:postid/comments/:commentid", check, async (req, res) => {
+  const { commentid } = req.params;
+  try {
+    const deleteResult = await Comment.deleteOne({ _id: commentid });
+
+    if (deleteResult.deletedCount === 1) {
+      return res.json({ _id: commentid });
+    } else {
+      res.status(500).json({
+        message: "An internal error occurred.",
+        details: ["Deleted count did not return 1."],
+      });
+    }
   } catch (err) {
     console.error(err);
     res.status(500).send();
